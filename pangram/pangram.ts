73926208b7e0928df8610e5b9141107de5ecad4f
Sean Gravener <sean@gravener.net>
2022-10-16 20:30:32 -0400

    Complete Pangram

--- pangram/pangram.ts
+++ pangram/pangram.ts
@@ -1,3 +1,11 @@
-export function isPangram() {
-  throw new Error('Remove this statement and implement this function')
+const startAt = 97; // https://www.w3schools.com/charsets/ref_utf_basic_latin.asp
+const glyphs = [...Array(26)].map((char, i) =>
+  String.fromCharCode(i + startAt)
+);
+
+export function isPangram(input: string): boolean {
+  input = input.toLowerCase();
+  const found = glyphs.map((char) => input.includes(char));
+
+  return found.length === 26 && !found.includes(false);
 }
